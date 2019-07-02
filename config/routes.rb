87eded31f94b58675d60d4a820d51ab6caded405
root <rohitshmahato@gmail.com>
2019-07-02 21:17:53 +0530

    Adding Post Controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
     controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
+
+  resources :posts, only: [:index, :show, :create] do
+      resources :photos, only: [:create]
+  end
 end
