e867bcfc0cb43bc858754c0b7f2dc4adb97ef1eb
root <rohitshmahato@gmail.com>
2019-07-15 10:51:27 +0530

    adding likes controllers

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,27 @@
+class LikesController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        @like = current_user.likes.build(like_params)
+        @post = @like.post
+        if @like.save
+            respond_to :js
+        else
+            flash[:alert] = "Something went wrong ..."
+        end
+
+    def destroy
+        @like = Like.find(params[:id])
+        @post = @like.post
+        if @like.destroy
+            respont_to :js
+        else
+            flash[:alert] = "Something went wrong ..."
+        end
+    end
+
+    private
+    def like_params
+        params.permit :post_id
+    end
+end
