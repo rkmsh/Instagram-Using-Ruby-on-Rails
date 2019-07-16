a47a3b56713aa2c5c0fea1709ab8a90494a21af6
root <rohitshmahato@gmail.com>
2019-07-16 08:13:22 +0530

    Working on Like View

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :photos, dependent: :destroy
-    has_many :likes, ~> {order(:created_at => :desc)}
+    has_many :likes, -> {order(:created_at => :desc)}
 
     def is_belongs_to? user
         Post.find_by(user_id: user.id, id: id)
